bc2954bc70140a71da6c71550bb8e607a21dfe31
Faiz-Rehman2 <155873425+Faiz-Rehman2@users.noreply.github.com>
2024-06-07 00:29:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -17,55 +17,55 @@ axios('https://fakestoreapi.com/products')
 
 
 
-// function filter(btn){
-//     axios('https://fakestoreapi.com/products')
-//     .then(responce => {
-//         div.innerHTML = ''
-//         console.log(btn.innerHTML)
-//         let filterdItems = responce.data.filter(item => item.category === btn.innerHTML)
-//         console.log(filterdItem)
-//         filterdItems.map(item => {
-//             div.innerHTML += `
-//             <img width='300' height='300' src="${item.image}" alt="...">
-//             <h2">Category : ${item.category}</h2>
-//             <h2>Price : $${item.price}</h2>
-//             <button>Add To Card</button>
-//             `
-//         })
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     })
-// }   
+function filter(btn){
+    axios('https://fakestoreapi.com/products')
+    .then(responce => {
+        div.innerHTML = ''
+        console.log(btn.innerHTML)
+        let filterdItems = responce.data.filter(item => item.category === btn.innerHTML)
+        console.log(filterdItem)
+        filterdItems.map(item => {
+            div.innerHTML += `
+            <img width='300' height='300' src="${item.image}" alt="...">
+            <h2">Category : ${item.category}</h2>
+            <h2>Price : $${item.price}</h2>
+            <button>Add To Card</button>
+            `
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}   
 
-// axios('https://fakestoreapi.com/products')
-//     .then(responce => {
-//         div.innerHTML = ''
-//         console.log(btn.innerHTML)
+axios('https://fakestoreapi.com/products')
+    .then(responce => {
+        div.innerHTML = ''
+        console.log(btn.innerHTML)
     
-//     res.data.filter(item =>item.category === btn.innerHTML).map(item =>{
-//         div.innerHTML += `
-//         
-//         <h1>Product name: ${item.brand} ${item.name}</h1>
-//         <h1>Category: ${item.category}</h1>
-//         <h1>Price: ${item.price}</h1>
-//         
-//         `
-//         })})
-// }
+    res.data.filter(item =>item.category === btn.innerHTML).map(item =>{
+        div.innerHTML += `
+        
+        <h1>Product name: ${item.brand} ${item.name}</h1>
+        <h1>Category: ${item.category}</h1>
+        <h1>Price: ${item.price}</h1>
+        
+        `
+        })})
+}
 
 
-// function item(){
-//     div.innerHTML = ''
-//     products.map(item => {
+function item(){
+    div.innerHTML = ''
+    products.map(item => {
 
-//         div.innerHTML += `
-//        
-//         <h1>Product name: ${item.brand} ${item.name}</h1>
-//         <h1>Category: ${item.category}</h1>
-//         <h1>Price: ${item.price}</h1>
-//         
-//         `
+        div.innerHTML += `
+       
+        <h1>Product name: ${item.brand} ${item.name}</h1>
+        <h1>Category: ${item.category}</h1>
+        <h1>Price: ${item.price}</h1>
+        
+        `
     
-//     })
-// }
+    })
+}
