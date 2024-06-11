a78a61f849cee69117b5e39ed5a448fe54a91b9a
Faiz-Rehman2 <FAIZREHMAN9o11@gmail.com>
2024-06-11 12:50:58 +0500

    last update

--- app.js
+++ app.js
@@ -1,71 +1,49 @@
-const div = document.querySelector('div');
+const div = document.querySelector('.cardItems');
+
+// Fetch and display all items initially
 axios('https://fakestoreapi.com/products')
-.then((res)=>{
-    console.log(res.data)
-    res.data.map((item)=>{
-        div.innerHTML +=`
-        <img width="300" src="${item.image}" alt="product.image"/>
-        <h1>Title:${item.category}</h1>
-        <h1>Price: ${item.price}</h1>
-        <button>Add to Cart</button>
-        </div>
-        `
-     })
-}).catch((err)=>{
-    console.log(err);
-})
-
-
-
-function filter(btn){
-    axios('https://fakestoreapi.com/products')
-    .then(responce => {
-        div.innerHTML = ''
-        console.log(btn.innerHTML)
-        let filterdItems = responce.data.filter(item => item.category === btn.innerHTML)
-        console.log(filterdItem)
-        filterdItems.map(item => {
-            div.innerHTML += `
-            <img width='300' height='300' src="${item.image}" alt="...">
-            <h2">Category : ${item.category}</h2>
-            <h2>Price : $${item.price}</h2>
-            <button>Add To Card</button>
-            `
-        })
+    .then((response) => {
+        displayItems(response.data);
     })
-    .catch(error => {
-        console.log(error);
-    })
-}   
+    .catch((err) => {
+        console.error(err);
+    });
 
-axios('https://fakestoreapi.com/products')
-    .then(responce => {
-        div.innerHTML = ''
-        console.log(btn.innerHTML)
-    
-    res.data.filter(item =>item.category === btn.innerHTML).map(item =>{
+// Function to display items
+function displayItems(items) {
+    div.innerHTML = '';
+    items.forEach((item) => {
         div.innerHTML += `
-        
-        <h1>Product name: ${item.brand} ${item.name}</h1>
-        <h1>Category: ${item.category}</h1>
-        <h1>Price: ${item.price}</h1>
-        
-        `
-        })})
+            <div class="cardBox">
+                <img src="${item.image}" alt="${item.title}">
+                <h2>Category: ${item.category}</h2>
+                <h3>Price: $${item.price}</h3>
+                <button id="addToCart">Add to Cart</button>
+            </div>
+        `;
+    });
 }
 
+// Filter items based on category
+function filteredItem(btn) {
+    const category = btn.innerHTML.toLowerCase();
+    axios('https://fakestoreapi.com/products')
+        .then((response) => {
+            const filteredItems = response.data.filter(item => item.category.toLowerCase() === category);
+            displayItems(filteredItems);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
 
-function item(){
-    div.innerHTML = ''
-    products.map(item => {
-
-        div.innerHTML += `
-       
-        <h1>Product name: ${item.brand} ${item.name}</h1>
-        <h1>Category: ${item.category}</h1>
-        <h1>Price: ${item.price}</h1>
-        
-        `
-    
-    })
+// Display all items when "All Items" button is clicked
+function item() {
+    axios('https://fakestoreapi.com/products')
+        .then((response) => {
+            displayItems(response.data);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 }
